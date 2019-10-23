55bc42defe5f608dd46a7757bda6aaecdfec6dd5
thayeray <thayeray@yahoo.com>
2019-10-23 15:52:36 -0400

    getting started

--- index.js
+++ index.js
@@ -7,9 +7,6 @@ app.get('/', function (req, res) {
 
 let port = process.env.PORT;
 if (port == null || port == "") { port = 8000; }
-/*app.listen(port, function () {
+app.listen(port, function () {
     console.log(`Example app listening on port ${port}!`);
-});*/
-app.listen(process.env.PORT || 3000, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-  });
+});
